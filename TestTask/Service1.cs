caf678521dcd4708d074bab10e9db6685b80c351
headstone <feuerschwanz1990@gmail.com>
2019-01-29 16:04:41 +0300

    Extract methods, add some comments

--- TestTask/Service1.cs
+++ TestTask/Service1.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using System.Management;
 using System.Security.AccessControl;
-using System.Security.Principal;
 using System.ServiceProcess;
 using Microsoft.Win32;
 
@@ -21,37 +18,48 @@ namespace TestTask
         {
             try
             {
-                var companyName = Registry.LocalMachine.CreateSubKey(@"SOFTWARE\Test company");
-                var productName = companyName.CreateSubKey("Test product");
-                productName.SetValue("URL", "localhost", RegistryValueKind.String);
-                Logger.Log("Key created");
-
-
-                var users = new List<string>();
-                var query = new SelectQuery("Win32_UserAccount");
-                var searcher = new ManagementObjectSearcher(query);
-                var skippedUser = "headstone";
-                foreach (ManagementObject envVar in searcher.Get())
-                {
-                    var username = (string) envVar["Name"];
-                    if (username == skippedUser) continue;
-                    users.Add($@"{Environment.MachineName}\\{username}");
-                }
-
-                var rs = new RegistrySecurity();
-                foreach (var user in users)
-                {
-                    rs.AddAccessRule(new RegistryAccessRule(user, RegistryRights.ReadKey, AccessControlType.Deny));
-                }
-
-                productName.SetAccessControl(rs);
-                Logger.Log("Permissions set");
+                var productName = CreateRegistryKey();
+                SetPermissions(productName);
             }
             catch (Exception e)
             {
                 Logger.Log(e.ToString());
                 throw;
             }
+        }
+
+        private static void SetPermissions(RegistryKey productName)
+        {
+            // Get all users except "skippedUser" 
+            var users = new List<string>();
+            var query = new SelectQuery("Win32_UserAccount");
+            var searcher = new ManagementObjectSearcher(query);
+            var skippedUser = "headstone";
+            foreach (ManagementObject envVar in searcher.Get())
+            {
+                var username = (string) envVar["Name"];
+                if (username == skippedUser) continue;
+                users.Add($@"{Environment.MachineName}\\{username}");
+            }
+
+            // Remove read permissions from all users in list
+            var rs = new RegistrySecurity();
+            foreach (var user in users)
+            {
+                rs.AddAccessRule(new RegistryAccessRule(user, RegistryRights.ReadKey, AccessControlType.Deny));
+            }
+
+            productName.SetAccessControl(rs);
+            Logger.Log("Permissions set");
+        }
+
+        private RegistryKey CreateRegistryKey()
+        {
+            var companyName = Registry.LocalMachine.CreateSubKey(@"SOFTWARE\Test company");
+            var productName = companyName.CreateSubKey("Test product");
+            productName.SetValue("URL", "localhost", RegistryValueKind.String);
+            Logger.Log("Key created");
+            return productName;
         }
 
         protected override void OnStop()
